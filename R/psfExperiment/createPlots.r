24cff87a86fae772e990a889411e680c862ed455
Jonas Schwammberger <1262464+lord-blueberry@users.noreply.github.com>
2019-10-24 15:22:14 +0200

    adding another experiment run

--- R/psfExperiment/createPlots.r
+++ R/psfExperiment/createPlots.r
@@ -13,14 +13,17 @@ readExperimet <- function(folder, experimentName, df){
     print(file)
     t = read.table(file.path(folder,file), header=TRUE, dec=",", sep=";")
     t["objective"] <- t["dataPenalty"] + t["regPenalty"]
-    t["experimentName"] = experimentName
     t["psfSize"] = as.integer(psfSize)
+    t["experimentName"] = experimentName
     df <- rbind(df, t)
   }
   
   return(df)
 }
 
+
+
+#-------------------------------------------------Generate plots for approximate deconvolution (simply cut the PSF in half and use this)
 table = read.table("psfExperiment/1Psf.txt", header=TRUE, dec=",", sep=";")
 table["objective"] <- table["dataPenalty"] + table["regPenalty"]
 table["experimentName"] = "Standard"
@@ -31,7 +34,7 @@ combined <- readExperimet("psfExperiment/psfSpeed", "Approx. deconvolution", tab
 combined["psfFraction"] <- combined["psfSize"]
 combined[, "psfSize"] <- as.factor(combined[, "psfSize"])
 
-png("./psfExperiment/plots/size.png",
+png("./psfExperiment/plots/ApproxDeconv/size.png",
     width = 6.0,
     height = 3.0,
     units = "in",
@@ -51,7 +54,7 @@ agg["timePerIter"] <- agg$time / as.numeric(agg$iter)
 agg["speedup"] <- agg$timePerIter[agg$psfFraction == 1] / agg$timePerIter
 agg["speedup_total"] <- agg$time[agg$psfFraction == 1] / agg$time
 
-png("./psfExperiment/plots/speedup_iter.png",
+png("./psfExperiment/plots/ApproxDeconv/speedup_iter.png",
     width = 3.0,
     height = 3.0,
     units = "in",
@@ -63,7 +66,63 @@ ggplot(data = agg, aes(x = psfFraction, y = speedup)) +
   scale_x_continuous(trans="log2")
 dev.off()
 
-png("./psfExperiment/plots/speedup_total.png",
+png("./psfExperiment/plots/ApproxDeconv/speedup_total.png",
+    width = 3.0,
+    height = 3.0,
+    units = "in",
+    res = 200)
+ggplot(data = agg, aes(x = psfFraction, y = speedup_total)) + 
+  xlab("PSF Fraction") +
+  ylab("Total Speedup") +
+  geom_line() + geom_point() +
+  scale_x_continuous(trans="log2")
+dev.off() 
+
+
+#-------------------------------------------------Generate plots for approximate Update (use a fraction of the psf to update the gradient map)
+table = read.table("psfExperiment/1Psf.txt", header=TRUE, dec=",", sep=";")
+table["objective"] <- table["dataPenalty"] + table["regPenalty"]
+table["experimentName"] = "Standard"
+table["psfSize"] = 1
+
+combined <- readExperimet("psfExperiment/psfSpeedApproxUpdate", "Approx. Update", table)
+#combined[, "psfSize"] <- imageSize * 1.0/as.numeric(combined[,"psfSize"])
+combined["psfFraction"] <- combined["psfSize"]
+combined[, "psfSize"] <- as.factor(combined[, "psfSize"])
+
+png("./psfExperiment/plots/ApproxUpdate/size.png",
+    width = 6.0,
+    height = 3.0,
+    units = "in",
+    res = 200)
+ggplot(data = combined, mapping = aes(x = cycle, y = objective, color=psfSize)) + 
+  xlab("Major cycle index") +
+  ylab("Objective value") +
+  scale_color_discrete(name = "PSF fraction", labels = paste("1 /" ,levels(combined$psfSize))) +
+  geom_line() + 
+  geom_point() + 
+  scale_y_continuous(trans= "log10")
+dev.off()
+
+timeGroup <- group_by(combined, psfFraction)
+agg <- summarize(timeGroup, time=sum(ElapsedTime), iter= sum(iterCount))
+agg["timePerIter"] <- agg$time / as.numeric(agg$iter)
+agg["speedup"] <- agg$timePerIter[agg$psfFraction == 1] / agg$timePerIter
+agg["speedup_total"] <- agg$time[agg$psfFraction == 1] / agg$time
+
+png("./psfExperiment/plots/ApproxUpdate/speedup_iter.png",
+    width = 3.0,
+    height = 3.0,
+    units = "in",
+    res = 200)
+ggplot(data = agg, aes(x = psfFraction, y = speedup)) + 
+  xlab("PSF Fraction") +
+  ylab("Per iteration Speedup") +
+  geom_line() + geom_point() +
+  scale_x_continuous(trans="log2")
+dev.off()
+
+png("./psfExperiment/plots/ApproxUpdate/speedup_total.png",
     width = 3.0,
     height = 3.0,
     units = "in",
