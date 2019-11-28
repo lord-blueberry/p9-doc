7d85f4b97197961b2313ed1f0675c26962c82203
Jonas Schwammberger <1262464+lord-blueberry@users.noreply.github.com>
2019-11-28 10:59:09 +0100

    before another major rework of radio interferometry intro

--- R/approx/approxPlots.r
+++ R/approx/approxPlots.r
@@ -5,32 +5,50 @@ imageSize <- 3072
 asinh <- scales::trans_new(name = 'asinh', transform = function(x) asinh(x*1000), 
                            inverse = function(x) sinh(x)/1000)
 
-readExperimet <- function(folder, file, experimentName, df){
-  print(file)
-  t = read.table(file.path(folder,file), header=TRUE, dec=",", sep=";")
-  t[,"cycle"] = as.factor(t$cycle)
-  t["experimentName"] = experimentName
-  df <- rbind(df, t)
-  
-  return(df)
-}
+scale_color_discrete(name = "PSF Fraction", labels = paste("1 /" ,levels(combined$psfSize)))
 
 folder <- "approx/PsfSize"
-file <- "PsfSize8.txt"
+file <- "PsfSize4.txt"
+outputfolder <- "./approx/output/"
+
 data = read.table(file.path(folder,file), header=TRUE, dec=",", sep=";")
 data[,"cycle"] = as.factor(data$cycle)
-data["experimentName"] = "psf8"
-for(i in c(16,32, 64)) {
+data["experimentName"] = "psf4"
+for(i in c(8, 16,32, 64)) {
   file <- paste("PsfSize", i, ".txt", sep="")
   data2 = read.table(file.path(folder,file), header=TRUE, dec=",", sep=";")
   data2[,"cycle"] = as.factor(data2$cycle)
   data2["experimentName"] = paste("psf",i,sep="")
   data <- rbind(data, data2)
 }
-
-outputfolder <- "./approx/output/"
-
 png(paste(outputfolder, "psfSize.png", sep=""),
+    width = 6.0,
+    height = 4.0,
+    units = "in",
+    res = 256)
+ggplot(data = data, mapping = aes(x = seconds, y = objectiveNormal, color= experimentName)) + 
+  xlab("time") +
+  ylab("Objective value") +
+  geom_line() +
+  scale_y_continuous(trans= "log10")+
+  scale_x_continuous(trans= "log10")+
+  coord_cartesian(ylim=c(30, 280.0))
+dev.off()
+
+
+folder <- "approx/BlockSize"
+file <- "block1.txt"
+data = read.table(file.path(folder,file), header=TRUE, dec=",", sep=";")
+data[,"cycle"] = as.factor(data$cycle)
+data["experimentName"] = "block 1"
+for(i in c(2, 4, 8)) {
+  file <- paste("block", i, ".txt", sep="")
+  data2 = read.table(file.path(folder,file), header=TRUE, dec=",", sep=";")
+  data2[,"cycle"] = as.factor(data2$cycle)
+  data2["experimentName"] = paste("block ",i,sep="")
+  data <- rbind(data, data2)
+}
+png(paste(outputfolder, "blockSize.png", sep=""),
     width = 6.0,
     height = 4.0,
     units = "in",
