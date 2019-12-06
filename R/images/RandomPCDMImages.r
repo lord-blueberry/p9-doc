7fb973dcd63c4799c654bd39497b0849a31ddebf
Jonas Schwammberger <1262464+lord-blueberry@users.noreply.github.com>
2019-12-06 13:56:50 +0100

    added more images

--- R/images/RandomPCDMImages.r
+++ R/images/RandomPCDMImages.r
@@ -57,3 +57,46 @@ ggplot(data, aes(x=x, y=y, fill=intensity))  +
   scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.50, r = -2, hue = 1.5, gamma = 0.9), guide=FALSE) +
   theme_void()
 dev.off()
+
+#--------------------------------------------------------------PCDM Comparison----------------------------------------------------------------------
+data <- read.table(paste(inputFolder, "SerialCD-N132.csv", sep=""), sep=";", header=TRUE, dec=",")
+png(paste(outputfolder, "SerialCD-N132.png", sep=""),
+    width = 6.0,
+    height = 5.0,
+    units = "in",
+    res = 200)
+ggplot(data, aes(x=x, y=y, fill=intensity))  +
+  geom_tile() +
+  scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.50, r = -2, hue = 1.5, gamma = 0.9))
+dev.off()
+data <- read.table(paste(inputFolder, "PCDM-N132.csv", sep=""), sep=";", header=TRUE, dec=",")
+png(paste(outputfolder, "PCDM-N132.png", sep=""),
+    width = 6.0,
+    height = 5.0,
+    units = "in",
+    res = 200)
+ggplot(data, aes(x=x, y=y, fill=intensity))  +
+  geom_tile() +
+  scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.50, r = -2, hue = 1.5, gamma = 0.9))
+dev.off()
+
+data <- read.table(paste(inputFolder, "SerialCD-Calibration.csv", sep=""), sep=";", header=TRUE, dec=",")
+png(paste(outputfolder, "SerialCD-Calibration.png", sep=""),
+    width = 6.0,
+    height = 5.0,
+    units = "in",
+    res = 200)
+ggplot(data, aes(x=x, y=y, fill=intensity))  +
+  geom_tile() +
+  scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.50, r = -2, hue = 1.5, gamma = 0.3))
+dev.off()
+data <- read.table(paste(inputFolder, "PCDM-Calibration.csv", sep=""), sep=";", header=TRUE, dec=",")
+png(paste(outputfolder, "PCDM-Calibration.png", sep=""),
+    width = 6.0,
+    height = 5.0,
+    units = "in",
+    res = 200)
+ggplot(data, aes(x=x, y=y, fill=intensity))  +
+  geom_tile() +
+  scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.50, r = -2, hue = 1.5, gamma = 0.3))
+dev.off()
