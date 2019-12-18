09b0ba76eee5c4f84b4750e38f4d93785dd6ce20
Jonas Schwammberger <1262464+lord-blueberry@users.noreply.github.com>
2019-12-18 14:41:12 +0100

    reworking results and PCDM

---
+++ R/images/iuwtImages.r
@@ -0,0 +1,75 @@
+#library(rasterVis)
+library(pals)
+library(ggplot2)
+library(reshape2)
+library(pixmap)
+
+asinh <- scales::trans_new(name = 'asinh', transform = function(x) asinh(x*1000), 
+                           inverse = function(x) sinh(x)/1000)
+
+
+inputFolder <- "./images/iuwt/"
+outputfolder <- "./images/output/iuwt/"
+
+data <- read.table(paste(inputFolder, "iuwt-model.csv", sep=""), sep=";", header=TRUE, dec=",")
+limit <- 0.0005
+negLimit <- -0.00001
+data$intensity[data$intensity < negLimit] = negLimit
+data$intensity[data$intensity > limit] = limit
+png(paste(outputfolder, "iuwt-model.png", sep=""),
+    width = 16.0,
+    height = 16.0,
+    units = "in",
+    res = 200)
+ggplot(data, aes(x=x, y=y, fill=intensity))  +
+  geom_tile() +
+  scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.0, r = -1.5, hue = 1.0, gamma = 0.5), guide=FALSE) + 
+  theme_void()
+dev.off()
+
+data <- read.table(paste(inputFolder, "iuwt-residuals.csv", sep=""), sep=";", header=TRUE, dec=",")
+png(paste(outputfolder, "iuwt-residuals.png", sep=""),
+    width = 9.0,
+    height = 8.0,
+    units = "in",
+    res = 200)
+ggplot(data, aes(x=x, y=y, fill=intensity))  +
+  geom_tile() +
+  scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.0, r = -1.5, hue = 1.0, gamma = 0.5), name="Jansky/Beam") +
+  theme_void()
+dev.off()
+
+data <- read.table(paste(inputFolder, "iuwt-Calibration.csv", sep=""), sep=";", header=TRUE, dec=",")
+data$x <- (data$x - min(data$x)) * 1.5 / 60.0
+data$y <- (data$y - min(data$y)) * 1.5 / 60.0
+limit <- 0.0005
+negLimit <- -0.00001
+data$intensity[data$intensity > limit] = limit
+data$intensity[data$intensity < negLimit] = negLimit
+png(paste(outputfolder, "iuwt-Calibration.png", sep=""),
+    width = 6.0,
+    height = 4.5,
+    units = "in",
+    res = 200)
+ggplot(data, aes(x=x, y=y, fill=intensity))  +
+  geom_tile() +
+  scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.0, r = -1.5, hue = 1.0, gamma = 0.5), name="Jansky/Beam") +
+  xlab("Ascension (arc minutes)") +
+  ylab("Declination (arc minutes)")
+dev.off()
+
+
+data <- read.table(paste(inputFolder, "iuwt-N132.csv", sep=""), sep=";", header=TRUE, dec=",")
+data$x <- (data$x - min(data$x)) * 1.5 / 60.0
+data$y <- (data$y - min(data$y)) * 1.5 / 60.0
+png(paste(outputfolder, "iuwt-N132.png", sep=""),
+    width = 6.0,
+    height = 4.5,
+    units = "in",
+    res = 200)
+ggplot(data, aes(x=x, y=y, fill=intensity))  +
+  geom_tile() +
+  scale_fill_gradientn(colors=cubehelix(n = 200, start = 0.0, r = -1.5, hue = 1.0, gamma = 0.9), name="Jansky/Beam") +
+  xlab("Ascension (arc minutes)") +
+  ylab("Declination (arc minutes)")
+dev.off()
